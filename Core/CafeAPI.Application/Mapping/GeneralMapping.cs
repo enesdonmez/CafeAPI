1ab051d7f6627bbc8873c82eb399d154f6e5c4e4
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-04-26 01:41:13 +0300

    add mapper

---
+++ Core/CafeAPI.Application/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CafeAPI.Application.Dtos.CategoryDtos;
+using CafeAPI.Application.Dtos.MenuItemDtos;
+using CafeAPI.Domain.Entities;
+
+namespace CafeAPI.Application.Mapping;
+
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Category,ResultCategoryDto>().ReverseMap();
+        CreateMap<Category,CreateCategoryDto>().ReverseMap();
+        CreateMap<Category,UpdateCategoryDto>().ReverseMap();
+        CreateMap<Category,DetailCategoryDto>().ReverseMap();
+
+        CreateMap<MenuItem, ResultMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, UpdateMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, DetailMenuItemDto>().ReverseMap();
+    }
+}
