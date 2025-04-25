c95aecfd861ef1618b7862afcfeecd5c3dbf39c9
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-04-26 01:43:05 +0300

    add registration extension method

---
+++ Core/CafeAPI.Application/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using CafeAPI.Application.Mapping;
+using CafeAPI.Application.Services.Abstract;
+using CafeAPI.Application.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CafeAPI.Application;
+
+public static class ServiceRegistration
+{
+    public static void AddApplicationServices(this IServiceCollection serviceCollection) 
+    { 
+        serviceCollection.AddAutoMapper(typeof(GeneralMapping));
+        serviceCollection.AddScoped<ICategoryService, CategoryService>();
+        serviceCollection.AddScoped<IMenuItemService, MenuItemService>();
+    }
+}
---
+++ Infrastructure/CafeAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using CafeAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CafeAPI.Persistence;
+
+public static class ServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(configuration.GetConnectionString("CafeDbConnection")));
+    }
+}
