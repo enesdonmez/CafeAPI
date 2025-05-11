8a90fb4c3d263036248a655f186ded1392bcf508
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-05-12 01:32:27 +0300

    Order and OrderItem Controllers

---
+++ Presentation/CafeAPI.Api/Controllers/OrderItemsController.cs
@@ -0,0 +1,42 @@
+﻿using CafeAPI.Application.Dtos.OrderItemDtos;
+using CafeAPI.Application.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeAPI.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderItemsController(IOrderItemItemService _orderItemItemService) : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrderItems()
+        {
+            var orderItems = await _orderItemItemService.GetAllOrderItems();
+            return CreateResponse(orderItems);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderItemById(int id)
+        {
+            var orderItem = await _orderItemItemService.GetOrderItemById(id);
+            return CreateResponse(orderItem);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderItem(CreateOrderItemDto createOrderItemDto)
+        {
+            var result = await _orderItemItemService.CreateOrderItem(createOrderItemDto);
+            return CreateResponse(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderItem(UpdateOrderItemDto updateOrderItemDto)
+        {
+            var result = await _orderItemItemService.UpdateOrderItem(updateOrderItemDto);
+            return CreateResponse(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderItem(int id)
+        {
+            var result = await _orderItemItemService.DeleteOrderItem(id);
+            return CreateResponse(result);
+        }
+    }
+}
---
+++ Presentation/CafeAPI.Api/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using CafeAPI.Application.Dtos.OrderDtos;
+using CafeAPI.Application.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeAPI.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrdersController(IOrderService _orderService) : BaseController
+    {
+      
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var orders = await _orderService.GetAllOrders();
+            return CreateResponse(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var order = await _orderService.GetOrderById(id);
+            return CreateResponse(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderDto createOrderDto)
+        {
+            var result = await _orderService.CreateOrder(createOrderDto);
+            return CreateResponse(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrder(UpdateOrderDto updateOrderDto)
+        {
+            var result = await _orderService.UpdateOrder(updateOrderDto);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var result = await _orderService.DeleteOrder(id);
+            return CreateResponse(result);
+        }
+    }
+}
